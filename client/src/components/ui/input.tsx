2c5bde4c33721017218956a335ce59bd6612cfa9
Dark Dreamweaver <jeffreylannen@gmail.com>
2026-04-22 21:50:04 -0700

    Fix import path for utility function 'cn'

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import { useDialogComposition } from "./components/ui/dialog";
 import { useComposition } from "./hooks/useComposition";
-import { cn } from "../lib/utils";
+import { cn } from "./lib/utils";
 import * as React from "react";
 
 function Input({
