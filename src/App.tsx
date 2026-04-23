7908cf03454f4a19dbc5a87adaf25ffd52042283
Dark Dreamweaver <jeffreylannen@gmail.com>
2026-04-22 18:38:39 -0700

    Change import path for ErrorBoundary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
+import { ErrorBoundary } from "../client/src/components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Workspace from "./pages/Workspace";
