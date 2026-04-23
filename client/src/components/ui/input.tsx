1e04366d9083a84783331aaa3faa425d1585d70a
Dark Dreamweaver <jeffreylannen@gmail.com>
2026-04-22 22:16:34 -0700

    Update input.tsx

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -1,4 +1,4 @@
-import { useDialogComposition } from "./components/ui/dialog";
+import { useDialogComposition } from "dialog";
 import { useComposition } from "../hooks/useComposition";
 import { cn } from "./lib/utils";
 import * as React from "react";
