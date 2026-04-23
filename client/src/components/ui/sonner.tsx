a3bf1911a3d37e0b70c9894665fbf7351cd9903e
Dark Dreamweaver <jeffreylannen@gmail.com>
2026-04-22 23:56:03 -0700

    Export toast function along with Toaster component

--- client/src/components/ui/sonner.tsx
+++ client/src/components/ui/sonner.tsx
@@ -1,6 +1,5 @@
-
 import { useTheme } from "next-themes";
-import { Toaster as Sonner, type ToasterProps } from "sonner";
+import { Toaster as Sonner, toast, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
@@ -21,4 +20,4 @@ const Toaster = ({ ...props }: ToasterProps) => {
   );
 };
 
-export { Toaster };
+export { Toaster, toast };
